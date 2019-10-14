90e2bdd00cb237ccc9d3155033881c1d6e5259c4
Tyler Kropiewnicki <tkotter8@gmail.com>
2019-10-14 10:49:59 -0400

    add clap, mod, and test app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap;
+
+mod copy;
+
 fn main() {
-    println!("Hello, world!");
+    // build app
+    let app = clap::App::new("otr");
+    // parse args
+    let matches = app.get_matches();
+    // test
+    println!("{:?}", matches);
+    println!("{}", copy::test());
 }
